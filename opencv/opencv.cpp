bfefdc612cccca7c8223dd9ca918b82e457c3b2e
Mr14L <a3262662@126.com>
2021-12-12 01:27:15 +0800

    2

--- opencv/opencv.cpp
+++ opencv/opencv.cpp
@@ -3,19 +3,138 @@
 
 #include <iostream>
 #include<opencv2\opencv.hpp>
+#include<math.h>
+#include "omp.h"
+#include<string>
+#include<stdio.h>
+#include <tchar.h> 
+#include<time.h>
+#include <ctime>
 using namespace cv;
 using namespace std;
 Mat grey;
+Mat result ;
+Mat result1;
+Mat out ;
 void test1() {
-    Mat result = grey.clone();
+   /* Mat result = grey.clone();
 
     for (int i = 1; i < grey.rows-1; i++) {
+        const uchar* previous = grey.ptr<uchar>(i - 1);
+        const uchar* current = grey.ptr<uchar>(i);
+        const uchar* next = grey.ptr<uchar>(i + 1);
+        uchar* output = result.ptr<uchar>(i);
         for (int j = 1; j < grey.cols-1; j++) {
-            result
+            output[j] = saturate_cast <uchar>(-1 * grey.ptr<uchar>(i - 1)[j - 1] + grey.ptr<uchar>(i - 1)[j + 1] - 2 * grey.ptr<uchar>(i)[j - 1] + 2 * grey.ptr<uchar>(i)[j + 1] - grey.ptr<uchar>(i)[j - 1] + grey.ptr<uchar>(i)[j + 1]);
+            
+        }
+    }
+    Mat result1= grey.clone();
+    for (int i = 1; i < grey.rows - 1; i++) {
+        const uchar* previous = grey.ptr<uchar>(i - 1);
+        const uchar* current = grey.ptr<uchar>(i);
+        const uchar* next = grey.ptr<uchar>(i + 1);
+        uchar* output = result1.ptr<uchar>(i);
+        for (int j = 1; j < grey.cols - 1; j++) {
+            output[j] = saturate_cast <uchar>(-1 * grey.ptr<uchar>(i - 1)[j - 1] - grey.ptr<uchar>(i - 1)[j + 1] - 2 * grey.ptr<uchar>(i-1)[j ] + 1 * grey.ptr<uchar>(i+1)[j + 1] +1* grey.ptr<uchar>(i+1)[j - 1] + 2*grey.ptr<uchar>(i)[j ]);
+
+        }
+    }
+    Mat out= grey.clone();
+    for (int i = 1; i < grey.rows - 1; i++) {
+        
+        uchar* output = out.ptr<uchar>(i);
+        for (int j = 1; j < grey.cols - 1; j++) {
+            output[j] = saturate_cast <uchar>(std::sqrt(std::pow(result.ptr<uchar>(i)[j],2)+std::pow(result1.ptr<uchar>(i)[j], 2)));
+
+        }
+    }*/
+   // imshow("测试程序", out);
+   // waitKey();
+   /* for (int i = 1; i < grey.rows - 1; i++) {
+        const uchar* previous = grey.ptr<uchar>(i - 1);
+        const uchar* current = grey.ptr<uchar>(i);
+        const uchar* next = grey.ptr<uchar>(i + 1);
+        uchar* output = result.ptr<uchar>(i);
+        uchar* output1 = result1.ptr<uchar>(i);
+        uchar* output2 = out.ptr<uchar>(i);
+        for (int j = 1; j < grey.cols - 1; j++) {
+            output[j] = saturate_cast <uchar>(-1 * grey.ptr<uchar>(i - 1)[j - 1] + grey.ptr<uchar>(i - 1)[j + 1] - 2 * grey.ptr<uchar>(i)[j - 1] + 2 * grey.ptr<uchar>(i)[j + 1] - grey.ptr<uchar>(i)[j - 1] + grey.ptr<uchar>(i)[j + 1]);
+            //printf("(%d,%d)Thread%d\n",i,j ,omp_get_thread_num());
+            output1[j] = saturate_cast <uchar>(-1 * grey.ptr<uchar>(i - 1)[j - 1] - grey.ptr<uchar>(i - 1)[j + 1] - 2 * grey.ptr<uchar>(i - 1)[j] + 1 * grey.ptr<uchar>(i + 1)[j + 1] + 1 * grey.ptr<uchar>(i + 1)[j - 1] + 2 * grey.ptr<uchar>(i)[j]);
+            output2[j] = saturate_cast <uchar>(std::sqrt(std::pow(result.ptr<uchar>(i)[j], 2) + std::pow(result1.ptr<uchar>(i)[j], 2)));
+        }
+    }*/
+    for (int i = 1; i < grey.rows - 1; i++) {
+        // const uchar* previous = grey.ptr<uchar>(i - 1);
+        // const uchar* current = grey.ptr<uchar>(i);
+        // const uchar* next = grey.ptr<uchar>(i + 1);
+         //uchar* output = result.ptr<uchar>(i);
+        // uchar* output1 = result1.ptr<uchar>(i);
+        uchar* output2 = out.ptr<uchar>(i);
+        for (int j = 1; j < grey.cols - 1; j++) {
+            //output[j] = saturate_cast <uchar>(-1 * grey.ptr<uchar>(i - 1)[j - 1] + grey.ptr<uchar>(i - 1)[j + 1] - 2 * grey.ptr<uchar>(i)[j - 1] + 2 * grey.ptr<uchar>(i)[j + 1] - grey.ptr<uchar>(i)[j - 1] + grey.ptr<uchar>(i)[j + 1]);
+            //printf("(%d,%d)Thread%d\n",i,j ,omp_get_thread_num());
+            //output1[j] = saturate_cast <uchar>(-1 * grey.ptr<uchar>(i - 1)[j - 1] - grey.ptr<uchar>(i - 1)[j + 1] - 2 * grey.ptr<uchar>(i - 1)[j] + 1 * grey.ptr<uchar>(i + 1)[j + 1] + 1 * grey.ptr<uchar>(i + 1)[j - 1] + 2 * grey.ptr<uchar>(i)[j]);
+            output2[j] = saturate_cast <uchar>(std::sqrt(std::pow(saturate_cast <uchar>(-1 * grey.ptr<uchar>(i - 1)[j - 1] + grey.ptr<uchar>(i - 1)[j + 1] - 2 * grey.ptr<uchar>(i)[j - 1] + 2 * grey.ptr<uchar>(i)[j + 1] - grey.ptr<uchar>(i)[j - 1] + grey.ptr<uchar>(i)[j + 1]), 2) + std::pow(saturate_cast <uchar>(-1 * grey.ptr<uchar>(i - 1)[j - 1] - grey.ptr<uchar>(i - 1)[j + 1] - 2 * grey.ptr<uchar>(i - 1)[j] + 1 * grey.ptr<uchar>(i + 1)[j + 1] + 1 * grey.ptr<uchar>(i + 1)[j - 1] + 2 * grey.ptr<uchar>(i)[j]), 2)));
         }
     }
 }
 void test2() {
+    Mat result = grey.clone();
+    #pragma omp parallel for
+    for (int i = 1; i < grey.rows - 1; i++) {
+        const uchar* previous = grey.ptr<uchar>(i - 1);
+        const uchar* current = grey.ptr<uchar>(i);
+        const uchar* next = grey.ptr<uchar>(i + 1);
+        uchar* output = result.ptr<uchar>(i);
+        for (int j = 1; j < grey.cols - 1; j++) {
+            output[j] = saturate_cast <uchar>(-1 * grey.ptr<uchar>(i - 1)[j - 1] + grey.ptr<uchar>(i - 1)[j + 1] - 2 * grey.ptr<uchar>(i)[j - 1] + 2 * grey.ptr<uchar>(i)[j + 1] - grey.ptr<uchar>(i)[j - 1] + grey.ptr<uchar>(i)[j + 1]);
+            //printf("(%d,%d)Thread%d\n",i,j ,omp_get_thread_num());
+        }
+    }
+    Mat result1 = grey.clone();
+    #pragma omp parallel for
+    for (int i = 1; i < grey.rows - 1; i++) {
+        const uchar* previous = grey.ptr<uchar>(i - 1);
+        const uchar* current = grey.ptr<uchar>(i);
+        const uchar* next = grey.ptr<uchar>(i + 1);
+        uchar* output = result1.ptr<uchar>(i);
+        for (int j = 1; j < grey.cols - 1; j++) {
+            output[j] = saturate_cast <uchar>(-1 * grey.ptr<uchar>(i - 1)[j - 1] - grey.ptr<uchar>(i - 1)[j + 1] - 2 * grey.ptr<uchar>(i - 1)[j] + 1 * grey.ptr<uchar>(i + 1)[j + 1] + 1 * grey.ptr<uchar>(i + 1)[j - 1] + 2 * grey.ptr<uchar>(i)[j]);
+
+        }
+    }
+    Mat out = grey.clone();
+    #pragma omp parallel for
+    for (int i = 1; i < grey.rows - 1; i++) {
+
+        uchar* output = out.ptr<uchar>(i);
+        for (int j = 1; j < grey.cols - 1; j++) {
+            output[j] = saturate_cast <uchar>(std::sqrt(std::pow(result.ptr<uchar>(i)[j], 2) + std::pow(result1.ptr<uchar>(i)[j], 2)));
+
+        }
+    }
+
+}
+void test3() {
+    
+#pragma omp parallel for
+    for (int i = 1; i < grey.rows - 1; i++) {
+       // const uchar* previous = grey.ptr<uchar>(i - 1);
+       // const uchar* current = grey.ptr<uchar>(i);
+       // const uchar* next = grey.ptr<uchar>(i + 1);
+        //uchar* output = result.ptr<uchar>(i);
+       // uchar* output1 = result1.ptr<uchar>(i);
+        uchar* output2 = out.ptr<uchar>(i);
+        for (int j = 1; j < grey.cols - 1; j++) {
+            //output[j] = saturate_cast <uchar>(-1 * grey.ptr<uchar>(i - 1)[j - 1] + grey.ptr<uchar>(i - 1)[j + 1] - 2 * grey.ptr<uchar>(i)[j - 1] + 2 * grey.ptr<uchar>(i)[j + 1] - grey.ptr<uchar>(i)[j - 1] + grey.ptr<uchar>(i)[j + 1]);
+            //printf("(%d,%d)Thread%d\n",i,j ,omp_get_thread_num());
+            //output1[j] = saturate_cast <uchar>(-1 * grey.ptr<uchar>(i - 1)[j - 1] - grey.ptr<uchar>(i - 1)[j + 1] - 2 * grey.ptr<uchar>(i - 1)[j] + 1 * grey.ptr<uchar>(i + 1)[j + 1] + 1 * grey.ptr<uchar>(i + 1)[j - 1] + 2 * grey.ptr<uchar>(i)[j]);
+            output2[j] = saturate_cast <uchar>(std::sqrt(std::pow(saturate_cast <uchar>(-1 * grey.ptr<uchar>(i - 1)[j - 1] + grey.ptr<uchar>(i - 1)[j + 1] - 2 * grey.ptr<uchar>(i)[j - 1] + 2 * grey.ptr<uchar>(i)[j + 1] - grey.ptr<uchar>(i)[j - 1] + grey.ptr<uchar>(i)[j + 1]), 2) + std::pow(saturate_cast <uchar>(-1 * grey.ptr<uchar>(i - 1)[j - 1] - grey.ptr<uchar>(i - 1)[j + 1] - 2 * grey.ptr<uchar>(i - 1)[j] + 1 * grey.ptr<uchar>(i + 1)[j + 1] + 1 * grey.ptr<uchar>(i + 1)[j - 1] + 2 * grey.ptr<uchar>(i)[j]), 2)));
+        }
+    }
+    
 
 }
 int main()
@@ -27,8 +146,26 @@ int main()
     
     cvtColor(picture, grey, COLOR_BGR2GRAY);;
     //cout << grey << endl;
-    imshow("测试程序", grey);
-    waitKey(20191117);
+    result = grey.clone();
+    result1 = grey.clone();
+    out = grey.clone();
+    clock_t t0 = clock();
+    test1();
+    clock_t t1 = clock();
+    printf("Time = %d\n", t1 - t0);
+    //clock_t t2 = clock();
+   // test2();
+  //  clock_t t3 = clock();
+   // printf("Time = %d\n", t3 - t2);
+    clock_t t4 = clock();
+    test3();
+    clock_t t5 = clock();
+    printf("Time = %d\n", t5 - t4);
+    cout << (double)(t1 - t0) / (t5 - t4) << endl;
+    //imshow("测试程序", out);
+    
+    //waitKey();
+    imwrite("22.jpg", out);
 }
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
 // 调试程序: F5 或调试 >“开始调试”菜单
